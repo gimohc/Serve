f5c58e6344b667d6dea2b14d63642295fa463295
gimohc <mrlenovog50@gmail.com>
2025-05-21 21:09:34 +0300

    Started working on store front page

--- app/(services)/store/[id].tsx
+++ app/(services)/store/[id].tsx
@@ -1,14 +1,42 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
+import { serviceProvider } from "@/app/_layout";
+import { colors } from "@/constants/colors";
+import MainMenuArrow from "@/components/mainMenuArrow";
 
 // local params return what this specific store provides
 
 const Store = () => {
   const { id } = useLocalSearchParams();
+  // get service provider by id
+  const provider: serviceProvider = {
+    type: "Any",
+    subTypes: ["service1", "service2", "service3", "service4"],
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS07eoWd2XKGJmiyNkO0kNa7JsoMpv3Ds8KlA&s",
+    address: "Amman, Al Waha Circle",
+    rating: 4,
+    apiID: "1",
+    name: "Store 1",
+  };
   return (
     <View>
-      <Text style={styles.pageTitle}> Store {id} </Text>
+      <View style={styles.header}>
+        <MainMenuArrow />
+        <Image style={styles.logo} source={{ uri: provider.logo }} />
+        <Text style={styles.pageTitle}> {provider.name} </Text>
+        <Pressable
+          style={styles.contact}
+          onPress={() => {
+            router.replace("/(tabs)/chat");
+          }}
+        >
+          {" "}
+          {/* create new contact */}
+          <Text> Contact Now </Text>
+        </Pressable>
+      </View>
+      <View> </View>
     </View>
   );
 };
@@ -21,6 +49,29 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 25,
     fontWeight: "bold",
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  logo: {
+    objectFit: "fill",
+    width: 75,
+    height: 75,
+    borderRadius: 35,
+    marginRight: 50,
+    marginLeft: 20,
+  },
+  header: {
+    flexDirection: "row",
+    position: "relative",
+    padding: 30,
+    backgroundColor: colors.DARKER_PRIMARY,
+  },
+  contact: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    padding: 7,
+    backgroundColor: colors.LIGHT_GRAY,
   },
   container: {},
 });
