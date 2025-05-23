2b28215cc23b01d2c5c113d084e8c7ae85f31cbd
gimohc <mrlenovog50@gmail.com>
2025-05-23 20:48:34 +0300

    Clarified MessageContext purpose

--- contexts/messageContext.tsx
+++ contexts/messageContext.tsx
@@ -1,6 +1,7 @@
 import { chatUser } from "@/components/chatUser";
 import { createContext, Dispatch, SetStateAction } from "react";
 
+// store current user chat opened, and set it
 export interface MessageContextProps {
     user: chatUser,
     setChatUser: Dispatch<SetStateAction<chatUser>>;
